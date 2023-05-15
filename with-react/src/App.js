3a9b8c3015363c39c0cc6a18a4a15401c6107ccd
Kaleb-Besufikad <se.kaleb.besufikad@gmail.com>
2023-05-15 21:26:19 +0300

    extra text removed

--- with-react/src/App.js
+++ with-react/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
 import {
   Login,
-  Register,
   Home,
   Home1,
   Shop,
@@ -17,22 +16,20 @@ import { register } from "swiper/element/bundle";
 register();
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/account_info" element={<AccountInfo />} />
-          <Route path="/home1" element={<Home1 />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/course_detail" element={<CourseDetail />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/account_info" element={<AccountInfo />} />
+        <Route path="/home1" element={<Home1 />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/course_detail" element={<CourseDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
